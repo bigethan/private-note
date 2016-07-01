b771ec38d582e43a8dfc516473d0561c8b8172de
Big Ethan <e@bigethan.com>
2016-07-01 10:07:53 +0100

    ability to clear list items

--- weird.sh
+++ weird.sh
@@ -9,18 +9,21 @@
 
 # choose your own logfile if you want
 LOGFILE=~/.weirdlog
+TEMPFILE=~/.weirdtmp
 
 #to prevent accidental exposure
 function rot13() {
   tr a-zA-Z n-za-mN-ZA-M
 }
 
-if [ $# -ne 0 ]; then
-  NOW=`date "+%Y-%m-%d %H:%M:%S |"`
-  LOG=`echo $NOW $@ | rot13`
+if [ "$1" = "done" ] && [ "$2" != "" ]; then
+  grep -v "^$2 |" $LOGFILE > $TEMPFILE && mv $TEMPFILE $LOGFILE
+elif [ $# -ne 0 ]; then
+  ID=`grep -o "^[0-9]\{1,\}" $LOGFILE| tail -1`
+  NOW=`date "+ | %Y-%m-%d %H:%M:%S |"`
+  LOG=`echo $(($ID + 1)) $NOW $@ | rot13`
   echo $LOG >> $LOGFILE
-  # to vusually hide what was just weirded
-  # still in the scrollback
+  # to _visually_ hide what was just entered
   clear
 else
   cat $LOGFILE | rot13
